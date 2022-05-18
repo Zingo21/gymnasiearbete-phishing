5ef596cb3d60d3dcc1379217f839111f8ba2e9e5
bohdan244 <100942221+bohdan244@users.noreply.github.com>
2022-05-18 18:41:37 +0200

    Delete check_login.php

--- check_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-# Startar sessionen
-session_start();
-
-# Om session-variabeln username inte finns
-if(!isset($_SESSION['username'])){
-    # Sicka till index.php med meddelande
-    header('location: index.php?mess=Du är inte inloggad.');
-}
